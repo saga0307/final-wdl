451bf87f772e8f00333e3523438851c6ab8fd176
saga0307 <74416754+saga0307@users.noreply.github.com>
2020-12-08 00:11:41 +0530

    Add files via upload

---
+++ hire.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="rent.css">
+  <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Vollkorn&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Vollkorn&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Armata&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Mada&family=Vollkorn&display=swap" rel="stylesheet">
+
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Vollkorn&display=swap" rel="stylesheet">
+
+</head>
+<style>
+  .bg{
+ 
+ background-size: cover;
+ height: 800px;
+overflow-x: hidden;
+
+  background-repeat: no-repeat; 
+ 
+}
+body{
+
+
+ 
+}
+.nav {
+
+  position: fixed;
+  float: right;
+
+/*  position: absolute; */
+  height: 30px;
+  
+    top: 0%;
+    left: 0%;
+
+    /*transform: translate(-50%, -50%);*/
+    padding: 100px;
+   margin-top: 50px;
+   margin-bottom: 30px;
+   margin-left: 20px;
+}
+
+
+input[type="datetime-local"]:before{
+    content: ' date & time';
+    color: #000;
+    text-align: center;
+    width:100%;
+}
+input[type="datetime-local"]:active:before, input[type="datetime-local"]:hover:before, input[type="datetime-local"]:visited:before,  input[type="datetime-local"]:focus:before{
+    content: '';
+    width: 100%;
+}
+</style>
+<body>
+
+<!----------------------------------------first nav--->
+     <div class="koko" style="margin-left:0px;padding-left: 60px; background-color: #eee; padding: 10px; margin-top:0px;">
+                <nav class="animated fadedown" >
+                      <a class="hi" href="videoback.php" id="welcome">Home</a>
+                      <a class="hi"href="huhu.php">Hire Car</a>
+                      <a class="hi"href="carde.php">Car Store</a>
+                      <a class="hi"href="#">Innovation</a>
+                      <a class="hi"href="#">About Us </a>
+                      <a class="hi"href="="  id="lo"> |  WorthWheels</a>
+                 </nav>
+      </div>
+
+<div class="fluid-container">
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+<div class="fluid-container" style=" background-color: #eee  ; height:600px; margin-top: -40px;" >
+
+
+        <div style="margin-left: 55%; margin-top: 40px;"  >
+
+          
+
+          <img src="carrent6.jpg" style=" height: 536px; width: 80%; margin-top: 15px; ">
+
+
+
+        </div>
+
+
+        <div class="caption" style=" width: 45%; margin-top: -300px;margin-left: 50px; height: 50%;  ">
+
+        <h2 style="text-align:center;margin-bottom: 150px;font-weight: 45px; font-size: 50px; font font-family: 'Vollkorn', serif;">"Best cars for the best journey"</h2>
+        
+        
+      </div>
+
+
+</div>
+
+<div class="container" style="padding: 80px ; border-bottom:1px solid grey"  data-aos="zoom-out">
+  <h2>The most practical and cost effective way is cars rental. Renting cars with different car body type to reach destination in most comfort is most convenient. Car rental industry changed the vision of people around the world to explore their favourite destination with vehicle type as per the need.</h2>
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+<!--form-->
+<div class=" fluid-container"  style="">
+
+
+
+
+  <div >
+    <img src="carform.jpg" style="margin-bottom:-950px; margin-left: 55px; padding-bottom: 150px"  data-aos="zoom-out">
+  </div>
+
+
+  <div class="main"  data-aos="zoom-out"style="margin-bottom: 300px; margin-top: 50px; ">
+
+      <div class="container"   style=" margin: 0 auto;  background-color:#eee;box-sizing: border-box;  margin-bottom:30px;box-shadow: 0px 2px 5px rgba(0,0,0,.9); width: 30%; margin-right:80px;height: 460px; float: right; margin-left:100px ;margin-top:200px;background: #fff;font-family: 'Vollkorn', serif;font-size: 20px; margin-top: 100px; margin-bottom: : 150px;">
+                <div class="container-form" style=" height:360px;padding-top:20px; padding-right:10px; padding-left: 30px;">
+                  <form action="#">
+                    <div class="form-field"  style="">
+                      <label style="margin-top:50px;"></label>
+
+                      <select name="select" id="#" style="margin-left:5px;padding-left: 50px; background-color: lightgrey; letter-spacing: 2px;height: 60px; border-bottom: 2px solid #71b8f3; width: 308px;">
+                        <option style=" background-color: white;"value="" disabled selected hidden>PICKUP</option>
+                        <option style=" background-color: white;"value="1">Borivali</option>
+                        <option style=" background-color: white;"value="2">Andheri</option>
+                        <option style=" background-color: white;"value="3">Bandra</option>
+                        <option style=" background-color: white;"value="4">Dadar</option>
+                        <option style=" background-color: white;"value="5">Churchgate</option>
+                      </select>
+                    </div>
+
+            <div class="form-field"  style="">
+                      <label style="margin-top:50px;"></label>
+
+                      <select name="select" id="#" style="margin-left:5px;padding-left: 50px; background-color: lightgrey; letter-spacing: 2px;height: 60px; border-bottom: 2px solid #71b8f3; width: 308px;">
+                        <option style=" background-color: white;"value="" disabled selected hidden>drop</option>
+                        <option style=" background-color: white;"value="1">Borivali</option>
+                        <option style=" background-color: white;"value="2">Andheri</option>
+                        <option style=" background-color: white;"value="3">Bandra</option>
+                        <option style=" background-color: white;"value="4">Dadar</option>
+                        <option style=" background-color: white;"value="5">Churchgate</option>
+                      </select>
+                    </div>
+
+
+
+
+
+
+                    <div class="form-field">
+                      
+                            <form action="/action_page.php">
+                              <!--<label  style="padding-top: 30px;margin: auto;" for="birthdaytime"></label>--
+                              <input placeholder="date & holder" style="margin-right: -40px; width:100px;border-bottom: 2px solid #71b8f3; margin: auto ;border-radius: 2px; width: 2px; margin-top: 30px;" type="datetime-local" id="birthdaytime" name="birthdaytime">-->
+                             <input style="width: 92%; margin-left:11px; margin-top: 20px;justify-content:  left; height: 60px; background-color: lightgrey;"  placeholder="date & holder" type="datetime-local" id="birthdaytime" name="birthdaytime">
+                             
+
+                              <!--<input style="width: 92%; margin-left:11px; margin-top: 20px; height: 60px;text-align: center; background-color: lightgrey;letter-spacing: 3px;"  placeholder="number of days" >-->
+
+                      <select name="select" id="#" style="width: 92%; padding-left:50px;margin-left:11px; margin-top: 20px; height: 60px;text-align: center; background-color: lightgrey;letter-spacing: 3px;"  placeholder="" >
+                        <option   style="padding-left: 50px; background-color: white; "value="" disabled selected hidden>number of days</option>
+                        <option   style="padding-left: 50px; background-color: white;" value="1">1 days </option>
+                        <option   style="padding-left: 50px; background-color: white;"value="2">2 days </option>
+                        <option   style="padding-left: 50px; background-color: white;" value="3">3 days </option>
+                        <option   style="padding-left: 50px; background-color: white;"value="4">4 days </option>
+                        <option   style="padding-left: 50px; background-color: white;"value="5">5 days</option>
+                        <option   style="padding-left: 50px; background-color: white;"value="5">1 Week</option>
+                         <option  style="padding-left: 50px; background-color: white;"value="5">2 Week</option>
+                      </select>
+                    </div>
+
+                            </form>
+                           <button type="submit"  style="margin-top: 29px;margin-left: 12px; width: 300px; height: 40px; background-color: lightgrey;background-color: black;color: white;border-color: white;padding-top: 5px; ">   <a href="types.php">Proceed</a> </button>
+                    </div>
+                    
+                   
+                  </form>
+             </div>
+         </div>
+
+</div>
+
+</div>
+
+<!--form-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+          <script>
+            AOS.init({
+              offset:400,
+              duration:600
+            });
+          </script>
+
+
+      </body>
+
+      </html>
